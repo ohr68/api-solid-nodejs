a94416d2c9656a178375ff4f34308d25c88dba93
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-07-15 20:16:55 -0300

    feat: :sparkles: Adiciona autenticação e configuração de test environment com vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,28 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
+    dir: 'src',
     coverage: {
       exclude: ['**/node_modules/**', '**/generated/**', '**/dist/**'],
     },
+    workspace: [
+      {
+        extends: true,
+        test: {
+          name: 'unit',
+          dir: 'src/tests/use-cases',
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: 'e2e',
+          dir: 'src/http/controllers',
+          environment:
+            './prisma/vitest-environment-prisma/prisma-test-environment.ts',
+        },
+      },
+    ],
   },
   plugins: [tsconfigPaths()],
 })
