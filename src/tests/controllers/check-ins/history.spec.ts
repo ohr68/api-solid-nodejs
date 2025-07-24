555028991e70353f64501749d45558dcb1f9fdfa
Otávio Odenik <otavio.odenik@etec.sp.gov.br>
2025-07-24 20:40:38 -0300

    refactor: :recycle: Refatora testes end to end para se adequar ao RBAC

--- src/tests/controllers/check-ins/history.spec.ts
+++ src/tests/controllers/check-ins/history.spec.ts
@@ -17,7 +17,7 @@ describe('Check-in History (e2e)', () => {
   })
 
   it('should be able to list the history of check-ins', async () => {
-    const { token } = await createAndAuthenticateUser(app)
+    const { token } = await createAndAuthenticateUser(app, true)
 
     const user = await prisma.user.findFirstOrThrow()
 
--- src/tests/utils/create-and-authenticate-user.ts
+++ src/tests/utils/create-and-authenticate-user.ts
@@ -1,16 +1,24 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'teste',
-    email: 'teste@teste.com',
-    password: '123455677',
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'teste',
+      email: 'teste@teste.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
 
   const auhtResponse = await request(app.server).post('/sessions').send({
     email: 'teste@teste.com',
-    password: '123455677',
+    password: '123456',
   })
 
   const { token } = auhtResponse.body
